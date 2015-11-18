e5fd9cd4bdc53ac625de4fa3e3243436154995ad
tonymtz <tmartinez@switchfly.com>
2015-11-17 23:25:42 -0700

    style updated (now is responsive)

--- app/components/inliner-.js
+++ app/components/inliner-.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
 
+var timeout;
+var lastTap = 0;
+
 export default Ember.Component.extend({
     classNames: ['input_text'],
 
     tagName: 'span',
+
+    touchEnd: function() {
+        let currentTime = new Date().getTime();
+        let tapLength = currentTime - lastTap;
+
+        clearTimeout(timeout);
+
+        if (tapLength < 500 && tapLength > 0) {
+            this.set('isEditing', true);
+            event.preventDefault();
+        } else {
+            timeout = setTimeout(function() {
+                clearTimeout(timeout);
+            }, 500);
+        }
+
+        lastTap = currentTime;
+    },
 
     doubleClick: function() {
         this.set('isEditing', true);
